2ccdf3de6a441c6748b21bf47bec860edc3c7628
meyling83 <116660994+meyling83@users.noreply.github.com>
2023-02-20 16:53:28 +0000

    cambio 2

--- adivinarEdad.py
+++ adivinarEdad.py
@@ -1,7 +1,13 @@
+#El programa adivina la edad del usuario. Para ello genera un numero aleatorio en el rango entre 1 y 120
+# le pregunta al usuario si esa es su edad.
+# si elusuario dice que sí, termina el programa, 
+# si dice que no hace un cilco en el cual pregunta al ausuario  si su edad es mayor que ese número,
+# si dice que sí, lo guarda en la variable min, sino lo guarda en la variable max, para ir acotando poco a poco el rango de la edad hasta que la adivina
+# el numero se va guardando 
 import random
 
 min=1
-max=99
+max=120
 x=random.randint(min,max)
 accion=input(f"Tu edad es {x} He adivinado? S/N")
 lista=[]
@@ -18,8 +24,6 @@ while accion.upper()=="N":
         accion=input(f"Tu edad es {x} He adivinado? S/N")
         lista.append(x)
      contador+=1
-     
-   
-
+ 
 print(f"Tu edad es {x}")
 print(f"He adivinado en  {contador} veces")
