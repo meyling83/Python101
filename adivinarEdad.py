367798a99c9c00f6d5af031873e43ca2bbd6d1c8
meyling83 <116660994+meyling83@users.noreply.github.com>
2023-02-20 16:40:01 +0000

    cambios 1

---
+++ adivinarEdad.py
@@ -0,0 +1,15 @@
+import random
+
+x=random.randint(1,99)
+accion=input(f"Tu edad es {x} He adivinado? S/N")
+while accion=="N":
+     mayor= input("Tu edad es mayor que {x}?  S/N")
+     if mayor=="S"
+        x=random.randint(x,99)
+     else
+        x=randint(1,x)
+     accion=input("Tu edad es {x} He adivinado? S/N")
+   
+
+
+print("fin de programa")
